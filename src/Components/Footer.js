dde41f79ec7d07e41766b5922c88eddc62ee34e3
goeteeks <goeteeks@gmail.com>
2021-11-07 14:16:22 -0500

    Removed a couple of things.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,11 +18,6 @@ class Footer extends Component {
               {networks}
            </ul>
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
         </div>
         
      </div>
